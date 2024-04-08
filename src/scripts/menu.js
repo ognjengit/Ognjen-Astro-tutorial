3d43ea463aac0a7186bd2699b9fef08a7a2df9ea
ognjengit <ognjen.trifunovic@sistemi.rs>
2024-04-08 11:11:26 +0200

    learning

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,5 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
+document.getElementById("hamburger").addEventListener("click", () => {
   document.querySelector(".nav-links").classList.toggle("expanded");
 });
+
+console.log(document.getElementById("hamburger"));
